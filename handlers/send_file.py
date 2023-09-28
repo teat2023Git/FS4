2389a575006af070878c1ff9b342a6dc4417520d
teat2023Git <145661969+teat2023Git@users.noreply.github.com>
2023-09-28 18:31:14 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -13,7 +13,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            f"❗❗𝗜𝗠𝗣𝗢𝗥𝗧𝗔𝗡𝗧❗❗ \n\nFiles will be deleted in 20 minutes to avoid copyright issues. \nPlease forward and save them.",
             disable_web_page_preview=True,
             quote=True
         )
@@ -37,7 +37,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 1800))
+    asyncio.create_task(delete_after_delay(sent_message, 1200))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
